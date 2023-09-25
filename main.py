b082f1a6f87f2e3c1aa256d16db5bf8b7e631d64
FabioKodland <145863466+FabioKodland@users.noreply.github.com>
2023-09-25 20:58:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+meme_dict = { "CRINGE": "Qualcosa di eccezionalmente strano o imbarazzante", 
+"LOL": "Una risposta comune a qualcosa di divertente",
+"ROFL": "ROFL è utilizzato come reazione a qualcosa di divertente, similea LOL"
+ } 
+parola = input("Scrivi una parola moderna che non capisci (usatutte le lettere maiuscole!): ") 
+if parola in meme_dict.keys():
+print(meme_dict[parola]) 
+else:
+ print("Non abbiamo ancora questa parola...Ma ci stiamo lavorando!")
