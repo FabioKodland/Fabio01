bbad083ba8971f5b855e5eda73b74a9595cc81df
FabioKodland <145863466+FabioKodland@users.noreply.github.com>
2023-10-11 13:34:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-meme_dict = { "CRINGE": "Qualcosa di eccezionalmente strano o imbarazzante", 
-"LOL": "Una risposta comune a qualcosa di divertente",
-"ROFL": "ROFL è utilizzato come reazione a qualcosa di divertente, similea LOL"
- } 
-parola = input("Scrivi una parola moderna che non capisci (usatutte le lettere maiuscole!): ") 
-if parola in meme_dict.keys():
-print(meme_dict[parola]) 
-else:
- print("Non abbiamo ancora questa parola...Ma ci stiamo lavorando!")
+import discord
+# import * - è un modo rapido per importare tutti i file della libreria.
+from bot_logic import gen_pass,gen_emodji,flip_coin
+
+# La variabile intents memorizza i privilegi del bot
+intents = discord.Intents.default()
+# Abilitazione del privilegio di lettura dei messaggi
+intents.message_content = True
+# Creare un bot nella variabile client e trasferirgli i privilegi
+client = discord.Client(intents=intents)
+
+
+# Una volta che il bot è pronto, stamperà il suo nome!
+@client.event
+async def on_ready():
+    print('Abbiamo effettuato l accesso come {client.user}')
+
+
+# Quando il bot riceve un messaggio, invia messaggi nello stesso canale!
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Ciao! Sono un bot!')
+
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    elif message.content.startswith('$emodji'):
+        await message.channel.send(gen_emodji()) 
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())        
+    else:
+        await message.channel.send("Non è possibile elaborare questo comando, mi dispiace!")
+
+client.run("MTE2MDUxNzMzMDc5OTk3MjQwMw.GNuLJI.kuxNNf_ZjcoqJrdMNZcO9fKwU33xPF-e4SGQaQ")
